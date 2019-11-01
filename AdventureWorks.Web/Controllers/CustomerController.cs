46e6a1c148523f7a896b20a152717d9c5cc09fd9
David Urting <david.urting@live.com>
2019-11-01 23:32:15 +0100

    Oefening 7.2

---
+++ AdventureWorks.Web/Controllers/CustomerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdventureWorks.Domain;
+using AdventureWorks.Domain.Models;
+using AdventureWorks.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdventureWorks.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // Tonen van een zoekcriteria scherm
+        public IActionResult Search()
+        {
+            SearchViewModel vm = new SearchViewModel();
+            return View("SearchCriteria", vm);
+        }
+
+        // Tonen van zoekresultaten
+        [HttpPost]
+        public IActionResult Search(string keyword)
+        {
+            CustomerManager cm = new CustomerManager();
+            var customers = cm.SearchCustomers(keyword)
+                .Select(c => new CustomerViewModel() 
+                { 
+                    Id = c.Id, 
+                    FirstName = c.FirstName, 
+                    LastName = c.LastName 
+                });
+            return View("SearchResults", customers);
+        }
+
+        // Tonen van details van een klant
+        public IActionResult Details(int id)
+        {
+            CustomerManager cm = new CustomerManager();
+            Customer c = cm.GetCustomer(id);
+            if (c != null)
+            {
+                CustomerViewModel vm = new CustomerViewModel()
+                {
+                    Id = c.Id,
+                    FirstName = c.FirstName,
+                    LastName = c.LastName
+                };
+                return View(vm);
+            }
+            else return NotFound();
+        }
+
+    }
+}
